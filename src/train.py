4c35d25a94faac808d006a049e3c6e83355d87c7
Kazuhito Go <gokazu@amazon.co.jp>
2022-03-18 13:19:21 +0000

    training is running

--- src/train.py
+++ src/train.py
@@ -1,7 +1,13 @@
+import os, subprocess
+print('install libsndfile1..')
+cmd_list = 'apt install libsndfile1 -y'.split(' ')
+res = subprocess.run(cmd_list, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+print(res.stdout.decode("utf8"))
+print(res.stderr.decode("utf8"))
+
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import itertools
-import os
 import time
 import argparse
 import json
@@ -17,6 +23,7 @@ from meldataset import MelDataset, mel_spectrogram, get_dataset_filelist
 from models import Generator, MultiPeriodDiscriminator, MultiScaleDiscriminator, feature_loss, generator_loss,\
     discriminator_loss
 from utils import plot_spectrogram, scan_checkpoint, load_checkpoint, save_checkpoint
+import subprocess
 
 torch.backends.cudnn.benchmark = True
 
@@ -230,11 +237,11 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--group_name', default=None)
-    parser.add_argument('--input_wavs_dir', default='LJSpeech-1.1/wavs')
+    parser.add_argument('--input_wavs_dir', default=os.path.join(os.environ.get('SM_CHANNEL_TRAINING'),'wavs'))
     parser.add_argument('--input_mels_dir', default='ft_dataset')
-    parser.add_argument('--input_training_file', default='LJSpeech-1.1/training.txt')
-    parser.add_argument('--input_validation_file', default='LJSpeech-1.1/validation.txt')
-    parser.add_argument('--checkpoint_path', default='cp_hifigan')
+    parser.add_argument('--input_training_file', default=os.path.join(os.environ.get('SM_CHANNEL_TRAINING'),'training.txt'))
+    parser.add_argument('--input_validation_file', default=os.path.join(os.environ.get('SM_CHANNEL_TRAINING'),'validation.txt'))
+    parser.add_argument('--checkpoint_path', default=os.environ.get('SM_MODEL_DIR'))
     parser.add_argument('--config', default='')
     parser.add_argument('--training_epochs', default=3100, type=int)
     parser.add_argument('--stdout_interval', default=5, type=int)
